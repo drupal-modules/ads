49045f6eb259aa1e9e4cf7aca0c954b73272e94e
nseam <adrian.kierzkowski@gmail.com>
2013-12-17 20:25:25 +0100

    ADS-46 Functional ads theme. Added few files for future use

---
+++ ads/templates/page.tpl.php
@@ -0,0 +1,9 @@
+<header>
+  <?php print render($page['header']); ?>
+</header>
+<content>
+  <?php print render($page['content']); ?>
+</content>
+<footer>
+  <?php print render($page['footer']); ?>
+</footer>
