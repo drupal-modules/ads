62d155839b8b105d7e1e98c2c74e013e6fa40db9
Adrian Kierzkowski <adrian.kierzkowski@gmail.com>
2014-06-09 16:27:51 +0200

    Added mysql setting reconfiguration into ads.dev.configure-site.pp puppet file [ADS-52]

--- puppet/ads.dev.configure-site.pp
+++ puppet/ads.dev.configure-site.pp
@@ -1,5 +1,5 @@
 #
-# Puppet recipe to install LAMP environment for ADS distribution.
+# Puppet recipe to configure fully functional ADS site, i.e. vhost, mysql configuration.
 #
 # Dependencies:
 #   sudo apt-get -y install puppet
@@ -36,4 +36,20 @@ apache::vhost { 'ads.localhost':
       allow_override => ['All'],
     },
   ],
+}
+
+class { '::mysql::server':
+  override_options => { 'mysqld' => {
+    'max_connections' => '1024',
+    'max_allowed_packet' => '100M',
+    'sort_buffer_size' => '50M',
+    'innodb_buffer_pool_size' => '400M',
+    'innodb_buffer_pool_instances' => '1',
+    'innodb_log_buffer_size' => '30M',
+    'innodb_read_io_threads' => '64',
+    'innodb_write_io_threads' => '64',
+    'innodb_io_capacity' => '5000',
+    'innodb_flush_log_at_trx_commit' => '2',
+    'innodb_log_file_size' => '800M'
+  } }
 }
