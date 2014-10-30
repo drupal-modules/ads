d78f22ae978ac344a701cc14afb275e9ccee7030
Adrian Kierzkowski <adrian.kierzkowski@gmail.com>
2014-10-30 15:47:51 +0100

    Moved ads theme into separate repository (into ads_theme repo). Refs mycognitive/ads_themes#1

--- ads/sass/overrides/README.md
+++
@@ -1,4 +0,0 @@
-Ads Template Overridess
-=========
-
-Please create folder for each overriden module (use module name) and place style overrides directly inside.
--- ads/template.php
+++ ads/template.php
@@ -1,44 +1,16 @@
 <?php
-
 /**
  * @file
- * ADS distribution theme.
+ * Contains the theme's functions to manipulate Drupal's default markup.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728096
  */
-
-function ads_theme_preprocess_page(&$vars) {
-  $output = '';
-  foreach (drupal_get_messages(NULL, TRUE) as $type => $messages) {
-    switch ($type) {
-      case 'error':
-        $icon = 'fa-exclamation';
-        break;
-
-      case 'warning':
-        $icon = 'fa-thumbs-o-down';
-        break;
-
-      case 'status':
-        $icon = 'fa-info';
-        break;
-    }
-
-    $output .= "<div class=\"messages $type\">\n";
-    $output .= " <ul>\n";
-    foreach ($messages as $message) {
-      $output .= '  <li><span class="icon"><i class="fa ' . $icon . '" ></i></span>' . $message . "</li>\n";
-    }
-    $output .= " </ul>\n";
-
-    $output .= "</div>\n";
-  }
-
-  $vars['messages'] = $output;
-}
 
 /**
  * Implements template_preprocess_panels_pane().
  */
-function ads_theme_preprocess_panels_pane(&$vars) {
+function ads_preprocess_panels_pane(&$vars) {
   if ($vars['pane']->type === 'ads_include_page') {
     $vars['theme_hook_suggestions'][] = 'panels_pane__' . $vars['pane']->configuration['page_name'];
   }
@@ -47,141 +19,129 @@ function ads_theme_preprocess_panels_pane(&$vars) {
 /**
  * Implements template_panels_default_style_render_region().
  */
-function ads_theme_panels_default_style_render_region($vars) {
+function ads_panels_default_style_render_region($vars) {
   return implode('', $vars['panes']);
 }
 
 /**
- * Returns HTML for an administrative block for display.
+ * Override or insert variables into the maintenance page template.
  *
  * @param $variables
- *   An associative array containing:
- *   - block: An array containing information about the block:
- *     - show: A Boolean whether to output the block. Defaults to FALSE.
- *     - title: The block's title.
- *     - content: (optional) Formatted content for the block.
- *     - description: (optional) Description of the block. Only output if
- *       'content' is not set.
- *
- * @ingroup themeable
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("maintenance_page" in this case.)
  */
-function ads_admin_block($variables) {
-
-  if (($library = libraries_detect('masonry')) && !empty($library['installed'])) {
-    libraries_load('masonry');
-    drupal_add_js('
-      jQuery(function() {jQuery(".masonry").masonry({
-        itemSelector: ".masonry-item",
-        columnWidth: 300,
-        isAnimated: true,
-      });});',
-      'inline'
-    );
-  }
-
-
-  $block = $variables['block'];
-  $output = '';
-
-  // Don't display the block if it has no content to display.
-  if (empty($block['show'])) {
-    return $output;
-  }
-
-  $output .= '<div class="admin-panel masonry-item">';
-  if (!empty($block['title'])) {
-    $output .= '<h3>' . $block['title'] . '</h3>';
-  }
-  if (!empty($block['content'])) {
-    $output .= '<span class="body">' . $block['content'] . '</span>';
-  }
-  else {
-    $output .= '<span class="description">' . $block['description'] . '</span>';
-  }
-  $output .= '</div>';
-
-  return $output;
-} 
+/* -- Delete this line if you want to use this function
+function ads_preprocess_maintenance_page(&$variables, $hook) {
+  // When a variable is manipulated or added in preprocess_html or
+  // preprocess_page, that same work is probably needed for the maintenance page
+  // as well, so we can just re-use those functions to do that work here.
+  ads_preprocess_html($variables, $hook);
+  ads_preprocess_page($variables, $hook);
+}
+// */
 
 /**
- * Returns HTML for the content of an administrative block.
+ * Override or insert variables into the html templates.
  *
  * @param $variables
- *   An associative array containing:
- *   - content: An array containing information about the block. Each element
- *     of the array represents an administrative menu item, and must at least
- *     contain the keys 'title', 'href', and 'localized_options', which are
- *     passed to l(). A 'description' key may also be provided.
- *
- * @ingroup themeable
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("html" in this case.)
  */
-function ads_admin_block_content($variables) {
-  $content = $variables['content'];
-  $output = '';
+/* -- Delete this line if you want to use this function
+function ads_preprocess_html(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 
-  if (!empty($content)) {
-    $class = 'admin-list';
-    if ($compact = system_admin_compact_mode()) {
-      $class .= ' compact';
-    }
-    $output .= '<span class="' . $class . '">';
-    foreach ($content as $item) {
-      $output .= '<span class="title">' . l($item['title'], $item['href'], $item['localized_options']) . '';
-      if (!$compact && isset($item['description'])) {
-        $output .= '<span class="description">' . filter_xss_admin($item['description']) . '</span>';
-      }
-      $output .= '</span>';
-    }
-    $output .= '</span>';
-  }
-  return $output;
+  // The body tag's classes are controlled by the $classes_array variable. To
+  // remove a class from $classes_array, use array_diff().
+  //$variables['classes_array'] = array_diff($variables['classes_array'], array('class-to-remove'));
 }
+// */
 
 /**
- * Returns HTML for an administrative page.
+ * Override or insert variables into the page templates.
  *
  * @param $variables
- *   An associative array containing:
- *   - blocks: An array of blocks to display. Each array should include a
- *     'title', a 'description', a formatted 'content' and a 'position' which
- *     will control which container it will be in. This is usually 'left' or
- *     'right'.
- *
- * @ingroup themeable
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
  */
-function ads_admin_page($variables) {
-
-  $blocks = $variables['blocks'];
-
-  $stripe = 0;
-  $container = array();
-
-  $i = 0;
-
-  $num_cols = 5;
-
-  for ($i = 1; $i <= 5; ++$i) {
-    $container[$i] = '';
-  }
-
-  foreach ($blocks as $block) {
-    if ($block_output = theme('admin_block', array('block' => $block))) {
-      //dsm ($block);
-      $container[$i = $i % $num_cols + 1] .= '<div class="menu-' . str_replace('/', '_', $block['tab_root']) . '">' . $block_output . '</div>';
-    }
-  }
-
-  $output = '<div class="admin clearfix">';
-  $output .= theme('system_compact_link');
-
-  $output .= '<div class="cols cols-' . $num_cols . '">';
-  foreach ($container as $id => $data) {
-    $output .= '<div class="col col-' . $id . '">';
-      $output .= $data;
-    $output .= '</div>';
-  }
-  $output .= '</div>';
-
-  $output .= '</div>';
-  return $output;
+/* -- Delete this line if you want to use this function
+function ads_preprocess_page(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
 }
+// */
+
+/**
+ * Override or insert variables into the node templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function ads_preprocess_node(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
+
+  // Optionally, run node-type-specific preprocess functions, like
+  // ads_preprocess_node_page() or ads_preprocess_node_story().
+  $function = __FUNCTION__ . '_' . $variables['node']->type;
+  if (function_exists($function)) {
+    $function($variables, $hook);
+  }
+}
+// */
+
+/**
+ * Override or insert variables into the comment templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("comment" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function ads_preprocess_comment(&$variables, $hook) {
+  $variables['sample_variable'] = t('Lorem ipsum.');
+}
+// */
+
+/**
+ * Override or insert variables into the region templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("region" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function ads_preprocess_region(&$variables, $hook) {
+  // Don't use Zen's region--sidebar.tpl.php template for sidebars.
+  //if (strpos($variables['region'], 'sidebar_') === 0) {
+  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
+  //}
+}
+// */
+
+/**
+ * Override or insert variables into the block templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("block" in this case.)
+ */
+/* -- Delete this line if you want to use this function
+function ads_preprocess_block(&$variables, $hook) {
+  // Add a count to all the blocks in the region.
+  // $variables['classes_array'][] = 'count-' . $variables['block_id'];
+
+  // By default, Zen will use the block--no-wrapper.tpl.php for the main
+  // content. This optional bit of code undoes that:
+  //if ($variables['block_html_id'] == 'block-system-main') {
+  //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('block__no_wrapper'));
+  //}
+}
+// */
