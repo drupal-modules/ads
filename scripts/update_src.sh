6f66d2efcc427a4d3afd42fe60f92b7168678742
Rafal <kenorb@gmail.com>
2014-11-07 18:10:02 +0000

    Improving script to update sources in the current dir when the script ran.

--- scripts/update_src.sh
+++ scripts/update_src.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script to update all git repositories to the recent version.
 
-CWD=$(dirname $0)
-PARENT="`dirname $CWD`"
-find $PARENT -name .git -type d -print0 | xargs -0 -L1 -I% -P4 sh -c 'cd -P -- "$(dirname -- "%")/" && pwd -P && git pull'
+#CWD=$(dirname $0)
+#PARENT="`dirname $CWD`"
+find . -name .git -type d -print0 | xargs -0 -L1 -I% -P4 sh -c 'cd -P -- "$(dirname -- "%")/" && pwd -P && git pull'
