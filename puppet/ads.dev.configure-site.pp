62c338b93179cfdd4c888878820cfb2d5ade53fd
Adrian Kierzkowski <adrian.kierzkowski@gmail.com>
2014-12-17 09:47:02 +0100

    Added Apache's "geoip" module to puppet's dependencies. Refs #115
    For testing on Travis we may use:
    
      - sudo touch /var/www/ads/index.php
      - sudo chown travis /var/www/ads/index.php
      - echo "<?php print_r(geoip_record_by_name('php.net')); ?>" > /var/www/ads/index.php
      - sudo wget -qO- http://ads.server/ | cat

--- puppet/ads.dev.configure-site.pp
+++ puppet/ads.dev.configure-site.pp
@@ -26,6 +26,39 @@ include apache::mod::expires
 include apache::mod::headers
 include apache::mod::php
 
+package { 'libapache2-mod-geoip' :
+  ensure => installed
+}
+
+class apache::mod::geoip {
+  ::apache::mod { 'geoip': }
+  # Template uses no variables
+  file { 'geoip.conf':
+    ensure  => file,
+    path    => "${::apache::mod_dir}/geoip.conf",
+    content => '
+      <IfModule mod_geoip.c>
+        GeoIPEnable On
+        GeoIPEnableUTF8 On 
+        # GeoIPOutput [Notes|Env|All]
+        GeoIPOutput All
+        GeoIPScanProxyHeaders On
+        <IfModule prefork.c>
+          GeoIPDBFile /usr/share/GeoIP/GeoIPCity.dat
+        </IfModule>
+        <IfModule !prefork>
+          GeoIPDBFile /usr/share/GeoIP/GeoIPCity.dat MMapCache
+        </IfModule>
+      </IfModule>
+      ',
+    require => Exec["mkdir ${::apache::mod_dir}"],
+    before  => File[$::apache::mod_dir],
+    notify  => Service['httpd'],
+  }
+}
+
+include apache::mod::geoip
+
 apache::vhost { 'ads.server':
   port    => '80',
 
